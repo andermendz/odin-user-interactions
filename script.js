316403bb7d89363f1acfda8bc6725c5974d32a1e
andermendz <andersonmendz19@gmail.com>
2024-06-02 21:26:15 -0500

    todo - add transitions to dropdown

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-let dropdownArea = document.querySelector('#dropdown-content')
-let dropdownOptions = document.querySelectorAll('.dropdown-option')
-let dropdownToggle = document.querySelector('#dropdown-toggle')
+let dropdownArea = document.querySelector("#dropdown-menu");
+let dropdownOptions = document.querySelectorAll(".dropdown-option");
+let dropdownToggle = document.querySelector("#dropdown-toggle");
 
 dropdownToggle.onclick = () => {
-
-
-
-   dropdownOptions.forEach((option) =>{
-
-    if (option.style.display == 'inline'){
-        option.style.display = 'none';
+  dropdownOptions.forEach((option) => {
+    if (option.style.display == "inline") {
+      option.style.display = "none";
+      dropdownArea.style.height = '3rem'
     } else {
-        option.style.display = 'inline';
+      option.style.display = "inline";
+      dropdownArea.style.height = '18rem'
     }
-    
-   })
-
-   
-   
-}
+  });
+};
