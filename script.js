2f007fad3fdf12e8ea12a7ae3dcffc6998ea0852
andermendz <andersonmendz19@gmail.com>
2024-06-02 21:05:38 -0500

    basic toggle on dropdown

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let dropdown = document.querySelector('#dropdown-content')
+let dropdownArea = document.querySelector('#dropdown-content')
+let dropdownOptions = document.querySelectorAll('.dropdown-option')
+let dropdownToggle = document.querySelector('#dropdown-toggle')
+
+dropdownToggle.onclick = () => {
+
+
+
+   dropdownOptions.forEach((option) =>{
+
+    if (option.style.display == 'inline'){
+        option.style.display = 'none';
+    } else {
+        option.style.display = 'inline';
+    }
+    
+   })
+
+   
+   
+}
