81c2203be794ee593b3ccc2a1988a86d1b7c500f
andermendz <andersonmendz19@gmail.com>
2024-06-03 21:08:36 -0500

    formatted code

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ let dropdownToggleSimbol = document.querySelector("#dropdown-toggle>span");
 
 dropdownToggle.onclick = () => {
   dropdownOptions.forEach((option) => {
-    if (option.classList.contains('option-hidden')) {
-      option.classList.remove('option-hidden');
-      option.classList.add('option-visible');
-      
-      dropdownToggleSimbol.innerHTML = 'keyboard_arrow_up';
-      
-      dropdownArea.style.height = '18rem'
+    if (option.classList.contains("option-hidden")) {
+      option.classList.remove("option-hidden");
+      option.classList.add("option-visible");
+
+      dropdownToggleSimbol.innerHTML = "keyboard_arrow_up";
+
+      dropdownArea.style.height = "18rem";
     } else {
-      option.classList.remove('option-visible');
-      option.classList.add('option-hidden');
+      option.classList.remove("option-visible");
+      option.classList.add("option-hidden");
 
-      dropdownToggleSimbol.innerHTML = 'keyboard_arrow_down';
+      dropdownToggleSimbol.innerHTML = "keyboard_arrow_down";
 
-      dropdownArea.style.height = '3rem'
+      dropdownArea.style.height = "3rem";
     }
   });
 };
